3febe49ff05cb3ef832a2fd544f38141c75d30cf
Aviral Dwivedi <144650488+Aviral-Dwivedi-0@users.noreply.github.com>
2024-03-05 15:45:37 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (70.91%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
         int rsum=0;
-        vector<int> answer;
         for(int i=0;i<nums.size();i++){
             rsum+=nums[i];
-            answer.push_back(rsum);
+            nums[i]=rsum;
         }
-        return answer;
+        return nums;
     }
 };
