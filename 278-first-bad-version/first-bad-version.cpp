81c3ff45ef78868e0c000c494e7b8cbf31e1beb5
Aviral Dwivedi <144650488+Aviral-Dwivedi-0@users.noreply.github.com>
2024-03-12 09:48:54 +0530

    Time: 131 ms (12.53%) | Memory: 7.3 MB (52.62%) - LeetSync

---
+++ 278-first-bad-version/first-bad-version.cpp
@@ -0,0 +1,14 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        for(int i=0;i<n;i++){
+            if(isBadVersion(i+1)){
+                return i+1;
+            }
+        }
+        return 0;
+    }
+};
