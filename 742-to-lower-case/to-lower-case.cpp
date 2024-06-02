7df06ad4c4bd759fab079b527f1236351f3c422d
Aviral Dwivedi <144650488+Aviral-Dwivedi-0@users.noreply.github.com>
2024-06-02 19:17:45 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (66.42%) - LeetSync

--- 742-to-lower-case/to-lower-case.cpp
+++ 742-to-lower-case/to-lower-case.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string toLowerCase(string s) {
         for(int i=0;i<s.length();i++){
-            s[i]=tolower(s[i]);
+            if(s[i]>=65 && s[i]<=90) s[i]+=32;
         }
         return s;
     }
