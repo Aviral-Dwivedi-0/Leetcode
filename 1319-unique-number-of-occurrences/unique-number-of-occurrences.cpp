7f337049f206225873396c1d80a60d5bcefdd5d8
Aviral Dwivedi <144650488+Aviral-Dwivedi-0@users.noreply.github.com>
2024-12-02 10:02:20 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (5.72%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -1,13 +1,29 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        vector<int> answer;
-        sort(arr.begin(),arr.end());
+        // METHOD 1
+
+        // vector<int> answer;
+        // sort(arr.begin(),arr.end());
+        // for(int i=0;i<arr.size();i++){
+        //     if(i==0 || arr[i]!=arr[i-1]) answer.push_back((count(arr.begin(),arr.end(),arr[i])));
+        // }
+        // set<int> checker(answer.begin(),answer.end());
+        // vector<int> check(checker.begin(),checker.end());
+        // return (answer.size()==check.size());
+
+        //METHOD 2
+        int hash[2001]={0};
         for(int i=0;i<arr.size();i++){
-            if(i==0 || arr[i]!=arr[i-1]) answer.push_back((count(arr.begin(),arr.end(),arr[i])));
+            hash[arr[i]+1000]++;
         }
-        set<int> checker(answer.begin(),answer.end());
-        vector<int> check(checker.begin(),checker.end());
-        return (answer.size()==check.size());
+        int checkhash[1001]={0};
+        for(int i=0;i<2001;i++){
+            checkhash[hash[i]]++;
+        }
+        for(int i=1;i<1001;i++){
+            if(checkhash[i]>1) return false;
+        }
+        return true;
     }
 };
