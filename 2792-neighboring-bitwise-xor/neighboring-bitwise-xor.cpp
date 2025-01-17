99f5b715e27f1315670f09a59c97814183ac4638
Aviral Dwivedi <144650488+Aviral-Dwivedi-0@users.noreply.github.com>
2025-01-17 14:58:10 +0530

    Time: 1 ms (70.56%) | Memory: 264 MB (48.13%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
     bool doesValidArrayExist(vector<int>& derived) {
-        int n=derived.size();
-        vector<int> original(n,0);
-        original[0]=0;
-        for(int i=0;i<derived.size();i++){
-            if(i!=n-1)
-            original[i+1]=derived[i]^original[i];
-            else 
-            original[0]=derived[n-1]^original[n-1];
-        }
-        if (original[0]==0) return true;
+        //SOLUTION 1
+
+        // int n=derived.size();
+        // vector<int> original(n,0);
+        // original[0]=0;
+        // for(int i=0;i<derived.size();i++){
+        //     if(i!=n-1)
+        //     original[i+1]=derived[i]^original[i];
+        //     else 
+        //     original[0]=derived[n-1]^original[n-1];
+        // }
+        // if (original[0]==0) return true;
 
 
-        original[0]=1;
-        for(int i=0;i<derived.size();i++){
-            if(i!=n-1)
-            original[i+1]=derived[i]^original[i];
-            else 
-            original[0]=derived[n-1]^original[n-1];
+        // original[0]=1;
+        // for(int i=0;i<derived.size();i++){
+        //     if(i!=n-1)
+        //     original[i+1]=derived[i]^original[i];
+        //     else 
+        //     original[0]=derived[n-1]^original[n-1];
+        // }
+        // if (original[0]==1) return true;
+        // return false;
+
+        //SOLUTION 2
+        int sum=0;
+        for(auto i:derived){
+            sum+=i;
         }
-        if (original[0]==1) return true;
-        return false;
+        return (sum%2==0);
     }
 };
