b69d94a2df71a90d7b9bf6328cd35dff7f99d9ed
Aviral Dwivedi <144650488+Aviral-Dwivedi-0@users.noreply.github.com>
2025-02-16 04:15:39 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (66.73%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,25 +1,45 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int hash[3]={0};
-        for(int i=0;i<nums.size();i++){
-            hash[nums[i]]++;
-        }
-        int index=0;
-        while(hash[0]>0 || hash[1]>0 || hash[2]>0){
-            if(hash[0]>0){
-                nums[index]=0;
-                hash[0]--;
+        // 1st Method
+        // int hash[3]={0};
+        // for(int i=0;i<nums.size();i++){
+        //     hash[nums[i]]++;
+        // }
+        // int index=0;
+        // while(hash[0]>0 || hash[1]>0 || hash[2]>0){
+        //     if(hash[0]>0){
+        //         nums[index]=0;
+        //         hash[0]--;
+        //     }
+        //     else if(hash[1]>0){
+        //         nums[index]=1;
+        //         hash[1]--;
+        //     }
+        //     else{
+        //         nums[index]=2;
+        //         hash[2]--;
+        //     }
+        //     index++;
+        // }
+
+        //2nd Method (Best) (Dutch National FlagAlgo)
+        int low=0;
+        int high=nums.size()-1;
+        int mid=0;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
             }
-            else if(hash[1]>0){
-                nums[index]=1;
-                hash[1]--;
+            else if(nums[mid]==1){
+                mid++;
             }
             else{
-                nums[index]=2;
-                hash[2]--;
+                swap(nums[mid],nums[high]);
+                high--;
             }
-            index++;
         }
     }
 };
