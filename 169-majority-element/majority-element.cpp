9107627e0a791de3523dea63f9dff7571eba0dfd
Aviral Dwivedi <144650488+Aviral-Dwivedi-0@users.noreply.github.com>
2025-02-16 04:27:57 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (58.33%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,7 +1,21 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        return nums[(nums.size())/2];
+        // Moore's Voting Algo
+        int counter=0;
+        int element;
+        for(int i=0;i<nums.size();i++){
+            if(counter==0){
+                element=nums[i];
+                counter++;
+            }
+            else if(nums[i]==element){
+                counter++;
+            }
+            else{
+                counter--;
+            }
+        }
+        return element;
     }
 };
