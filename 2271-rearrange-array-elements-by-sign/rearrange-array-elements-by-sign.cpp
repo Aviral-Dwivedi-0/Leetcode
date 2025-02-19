421490645d9ed9b126dbbdeef5d24b7065305908
Aviral Dwivedi <144650488+Aviral-Dwivedi-0@users.noreply.github.com>
2025-02-20 05:12:02 +0530

    Time: 8 ms (52.42%) | Memory: 128.2 MB (94.72%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -5,27 +5,37 @@ public:
         vector<int> answer(n,0);
         int even=0;
         int odd=1;
-        int i=0;
-        while(even<n && odd<n){
+        // int i=0;
+        // while(even<n && odd<n){
+        //     if(nums[i]>0){
+        //         answer[even]=nums[i];
+        //         even+=2;
+        //     }
+        //     else {
+        //         answer[odd]=nums[i];
+        //         odd+=2;
+        //     }
+        //     i++;
+        // }
+        // while(even<=n-1){
+        //     answer[even]=nums[i];
+        //     i++;
+        //     even+=2;
+        // }
+        // while(odd<=n-1){
+        //     answer[odd]=nums[i];
+        //     i++;
+        //     odd+=2;
+        // }
+        for(int i=0;i<nums.size();i++){
             if(nums[i]>0){
                 answer[even]=nums[i];
                 even+=2;
             }
-            else {
+            else{
                 answer[odd]=nums[i];
                 odd+=2;
             }
-            i++;
-        }
-        while(even<=n-1){
-            answer[even]=nums[i];
-            i++;
-            even+=2;
-        }
-        while(odd<=n-1){
-            answer[odd]=nums[i];
-            i++;
-            odd+=2;
         }
         return answer;
     }
