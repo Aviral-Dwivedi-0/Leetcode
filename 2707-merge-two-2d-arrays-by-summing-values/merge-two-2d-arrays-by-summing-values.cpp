9e546d8ce70a4d13a7df7504969e78b4670e9f12
Aviral Dwivedi <144650488+Aviral-Dwivedi-0@users.noreply.github.com>
2025-03-02 15:47:27 +0530

    Time: 7 ms (6.81%) | Memory: 14.8 MB (44.73%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
+        // METHOD 1
+
         vector<vector<int>> answer;
         int i=0;
         int j=0;
@@ -10,8 +12,6 @@ public:
         vector<int> temp;
         while(i<n && j<m){
             if(nums1[i][0]==nums2[j][0]){
-                // answer[k].push_back(nums1[i][0]);
-                // answer[k].push_back(nums1[i][1]+nums2[j][1]);
                 temp.push_back(nums1[i][0]);
                 temp.push_back(nums1[i][1]+nums2[j][1]);
                 answer.push_back(temp);
@@ -40,5 +40,29 @@ public:
             j++;
         }
         return answer;
+
+        //METHOD 2
+        // int hash[201]={0};
+        // int n=nums1.size();
+        // int m=nums2.size();
+        // for(int i=0;i<n;i++){
+        //     hash[nums1[i][0]]+=nums1[i][1];
+        // }
+        // for(int j=0;j<m;j++){
+        //     hash[nums2[j][0]]+=nums2[j][1];
+        // }
+        // vector<vector<int>> answer;
+        // vector<int> temp;
+        // for(int i=0;i<201;i++){
+        //     if(hash[i]>0){
+        //         // temp.push_back(i);
+        //         // temp.push_back(hash[i]);
+        //         // answer.push_back(temp);
+        //         // temp.pop_back();
+        //         // temp.pop_back();
+        //         answer.push_back({i,hash[i]});
+        //     }
+        // }
+        // return answer;
     }
 };
