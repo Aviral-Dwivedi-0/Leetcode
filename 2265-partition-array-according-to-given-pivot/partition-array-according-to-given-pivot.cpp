6e682950a6210627650e527107870babfa8d6a93
Aviral Dwivedi <144650488+Aviral-Dwivedi-0@users.noreply.github.com>
2025-03-03 20:29:31 +0530

    Time: 2 ms (90.85%) | Memory: 127.6 MB (96.86%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,26 +1,50 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int> before;
-        vector<int> after;
-        int counter=0;
-        for(auto i: nums){
+        //METHOD 1
+
+        // vector<int> before;
+        // vector<int> after;
+        // int counter=0;
+        // for(auto i: nums){
+        //     if(i<pivot){
+        //         before.push_back(i);
+        //     }
+        //     else if(i>pivot){
+        //         after.push_back(i);
+        //     }
+        //     else{
+        //         counter++;
+        //     }
+        // }
+        // for(int i=0;i<counter;i++){
+        //     before.push_back(pivot);
+        // }
+        // for(int i=0;i<after.size();i++){
+        //     before.push_back(after[i]);
+        // }
+        // return before;
+
+        //METHOD 2
+        int n=nums.size();
+        vector<int> answer(n,0);
+        int left=0;
+        int right=n-1;
+        for(auto i:nums){
             if(i<pivot){
-                before.push_back(i);
+                answer[left]=i;
+                left++;
             }
             else if(i>pivot){
-                after.push_back(i);
-            }
-            else{
-                counter++;
+                answer[right]=i;
+                right--;
             }
         }
-        for(int i=0;i<counter;i++){
-            before.push_back(pivot);
+        reverse(answer.begin()+right+1,answer.end());
+        while(left<=right){
+            answer[left]=pivot;
+            left++;
         }
-        for(int i=0;i<after.size();i++){
-            before.push_back(after[i]);
-        }
-        return before;
+        return answer;
     }
 };
