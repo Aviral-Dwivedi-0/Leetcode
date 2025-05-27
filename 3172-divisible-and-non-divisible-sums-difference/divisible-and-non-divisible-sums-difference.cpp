1f5e8d1349ab5d050077dd82c83ccffaa42ccd92
Aviral Dwivedi <144650488+Aviral-Dwivedi-0@users.noreply.github.com>
2025-05-27 14:57:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (94.16%) - LeetSync

--- 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.cpp
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.cpp
@@ -4,18 +4,14 @@ public:
         int sum1=0;
         int sum2=0;
         int i=1;
-        int j=1;
         while(i<=n){
             if(i%m!=0){
                 sum1+=i;
             }
-            i++;
-        }
-        while(j<=n){
-            if(j%m==0){
-                sum2+=j;
+            else{
+                sum2+=i;
             }
-            j++;
+            i++;
         }
         return sum1-sum2;
     }
