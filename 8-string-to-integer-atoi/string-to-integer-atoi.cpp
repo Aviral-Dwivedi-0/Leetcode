322cc2b7888fa5c46a251a7e450dc8cd5c9ad129
Aviral Dwivedi <144650488+Aviral-Dwivedi-0@users.noreply.github.com>
2025-06-08 02:18:56 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (14.32%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        while(s[0]==' '){
+            s.erase(0,1);
+        }
+        int l=0;
+        // while(s[0]=='+' || s[0]=='-' || s[l]>='0' && s[l]<='9'){
+        //     l++;
+        // }
+        // if(l>10){
+        //     if(s[0]=='-') return pow(2,31)*-1;
+        //     else return pow(2,31);
+        // }
+        // s=s.substr(0,l);
+        
+        if(s[0]=='-'){
+            long long answer=0;
+            // if(s.size()>11) return pow(2,31)*-1;
+            for(int i=1;i<s.size();i++){
+                if(s[i]<48 || s[i]>57) break;
+                answer= answer*10+(s[i]-'0');
+                if(log10(abs(answer))>11) return(pow(2,31)*-1);
+            }
+            answer*=-1;
+            if(answer>= -1*(pow(2,31)))
+            return answer;
+            else return -1*(pow(2,31));
+        }
+        else{
+            if(s[0]=='+') s.erase(0,1);
+            // if(s.size()>11) return pow(2,31)-1;
+            long long answer=0;
+            for(auto i:s){
+                if(i<48 || i>57) break;
+                answer= answer*10+(i-'0');
+                if(log10(abs(answer))>11) return(pow(2,31)-1);
+            }
+            if(answer<pow(2,31))
+            return answer;
+            else return pow(2,31)-1;
+
+        }
+        return 0;
+    }
+};
