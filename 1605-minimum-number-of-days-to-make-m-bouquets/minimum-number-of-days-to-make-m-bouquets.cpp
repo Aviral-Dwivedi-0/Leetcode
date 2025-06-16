ba79d9b872ba18b26c60cea9805de21575bd19aa
Aviral Dwivedi <144650488+Aviral-Dwivedi-0@users.noreply.github.com>
2025-06-17 01:24:21 +0530

    Time: 23 ms (57.35%) | Memory: 70.4 MB (98.06%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -3,8 +3,6 @@ public:
     int minDays(vector<int>& bloomDay, int m, int k) {
         long long n=bloomDay.size();
         if(n<(long long)m*(long long)k) return -1;
-        // int high=*max_element(bloomDay.begin(),bloomDay.end());
-        // int low=*min_element(bloomDay.begin(),bloomDay.end());
         int high=INT_MIN;
         int low=INT_MAX;
         for(int i:bloomDay){
@@ -13,27 +11,44 @@ public:
         }
         while(low<=high){
             int mid=low+(high-low)/2;
-            int canmake=0;
-            int sum=0;
+            int counter=0;
+            int numberofBouquets=0;
             for(int i=0;i<n;i++){
-                if(mid>=bloomDay[i]){
-                    sum++;
-                    if(sum==k){
-                        canmake++;
-                        sum=0;
-                        if(canmake==m){
-                            high=mid-1;
-                            break;
-                        }
-                    }
-                } 
+                if(bloomDay[i]<=mid){
+                    counter++;
+                }
                 else{
-                    sum=0;
+                    numberofBouquets+=(counter/k);
+                    counter=0;
                 }
             }
-            if(canmake!=m){
-                low=mid+1;
-            }
+            numberofBouquets+=(counter/k);
+            if(numberofBouquets>=m) high=mid-1;
+            else low=mid+1;
+
+
+            //APPROACH 2
+            // int canmake=0;
+            // int sum=0;
+            // for(int i=0;i<n;i++){
+            //     if(mid>=bloomDay[i]){
+            //         sum++;
+            //         if(sum==k){
+            //             canmake++;
+            //             sum=0;
+            //             if(canmake==m){
+            //                 high=mid-1;
+            //                 break;
+            //             }
+            //         }
+            //     } 
+            //     else{
+            //         sum=0;
+            //     }
+            // }
+            // if(canmake!=m){
+            //     low=mid+1;
+            // }
         }
         return low;
     }
