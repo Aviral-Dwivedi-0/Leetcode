64f0859c2b46993259a21be533f9ff32214ce5f7
Aviral Dwivedi <144650488+Aviral-Dwivedi-0@users.noreply.github.com>
2025-06-17 01:47:02 +0530

    Time: 13 ms (33.27%) | Memory: 26.1 MB (47.72%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     int smallestDivisor(vector<int>& nums, int threshold) {
-        int low=1;
-        int high=*max_element(nums.begin(),nums.end());
+        int low=1, high=INT_MIN;
+        for(int i:nums){
+            if(i>high) high=i;
+        }
         while(low<=high){
             int mid=low+(high-low)/2;
             int divisors=0;
