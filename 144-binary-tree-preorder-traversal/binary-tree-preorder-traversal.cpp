274310bf7103bb881990d13a35c145e0403a6b73
Aviral Dwivedi <144650488+Aviral-Dwivedi-0@users.noreply.github.com>
2025-06-23 11:36:09 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (15.28%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -12,13 +12,30 @@
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
-        if(root==nullptr) return {};
+        // if(root==nullptr) return {};
+        // vector<int> answer;
+        // answer.push_back(root->val);
+        // vector<int> left=preorderTraversal(root->left);
+        // vector<int> right=preorderTraversal(root->right);
+        // answer.insert(answer.end(),left.begin(),left.end());
+        // answer.insert(answer.end(),right.begin(),right.end());
+        // return answer;
+        TreeNode* temp=root;
+        if(temp==nullptr) return {};
         vector<int> answer;
-        answer.push_back(root->val);
-        vector<int> left=preorderTraversal(root->left);
-        vector<int> right=preorderTraversal(root->right);
-        answer.insert(answer.end(),left.begin(),left.end());
-        answer.insert(answer.end(),right.begin(),right.end());
+        stack<TreeNode*> st;
+        st.push(temp);
+        while(!st.empty()){
+            TreeNode* number=st.top();
+            st.pop();
+            answer.push_back(number->val);
+            if(number->right){
+                st.push(number->right);
+            }
+            if(number->left){
+                st.push(number->left);
+            }
+        }
         return answer;
     }
 };
