13121a7451cb9198ff14e9aa5fa5b8b96d862866
Aviral Dwivedi <144650488+Aviral-Dwivedi-0@users.noreply.github.com>
2025-09-14 21:38:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (48.51%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size()!=t.size()) return false;
-        int hash1[26]={0};
-        int hash2[26]={0};
+        int hash[26]={0};
+        int goal[26]={0};
         for(int i=0;i<s.size();i++){
-            hash1[s[i]-'a']++;
-            hash2[t[i]-'a']++;
+            hash[s[i]-'a']++;
+        }
+        for(int j=0;j<t.size();j++){
+            goal[t[j]-'a']++;
         }
         for(int i=0;i<26;i++){
-            if(hash1[i]!=hash2[i]) return false;
+            if(hash[i]!=goal[i]) return false;
         }
         return true;
     }
