7eb5f7c95ca8d9c6e45f973e4f907d446448a8aa
Aviral Dwivedi <144650488+Aviral-Dwivedi-0@users.noreply.github.com>
2025-11-17 02:41:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (21.91%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,36 +1,45 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        
-        while(s[0]==' '){
+        if(s[0]==' '){
+            int first=-1;
+            for(int i=0;i<s.size();i++){
+                if(s[i]!=' '){
+                    first=i;
+                    break;
+                }
+            }
+            s.erase(0,first);
+        }
+        bool negative=0;
+        if(s[0]=='-' || s[0]=='+'){
+            if(s[0]=='-')
+            negative=true;
             s.erase(0,1);
         }
-        int l=0;        
-        if(s[0]=='-'){
-            long long answer=0;
-            for(int i=1;i<s.size();i++){
-                if(s[i]<48 || s[i]>57) break;
-                answer= answer*10+(s[i]-'0');
-                if(log10(abs(answer))>11) return(pow(2,31)*-1);
+        
+        if(s[0]==0){
+            int first=-1;
+            for(int i=0;i<s.size();i++){
+                if(s[i]!=0){
+                    first=i;
+                    break;
+                }
             }
-            answer*=-1;
-            if(answer>= -1*(pow(2,31)))
-            return answer;
-            else return -1*(pow(2,31));
+            s.erase(0,first);
         }
-        else{
-            if(s[0]=='+') s.erase(0,1);
-            long long answer=0;
-            for(auto i:s){
-                if(i<48 || i>57) break;
-                answer= answer*10+(i-'0');
-                if(log10(abs(answer))>11) return(pow(2,31)-1);
+        long long answer=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]-'0'>9 || s[i]-'0'<0) break;
+            else{
+                answer*=10;
+                if(answer>INT_MAX) break;
+                answer+=s[i]-'0';
             }
-            if(answer<pow(2,31))
-            return answer;
-            else return pow(2,31)-1;
-
         }
-        return 0;
+        if(negative) answer= answer*-1;
+        if(answer<INT_MIN) return INT_MIN;
+        if(answer>INT_MAX) return INT_MAX;
+        return answer;
     }
 };
