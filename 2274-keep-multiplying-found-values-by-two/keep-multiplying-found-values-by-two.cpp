84bce6ac8a4ebad4dac8d57d26a664befaa6dde2
Aviral Dwivedi <144650488+Aviral-Dwivedi-0@users.noreply.github.com>
2025-11-19 18:59:30 +0530

    Time: 6 ms (18.80%) | Memory: 15.8 MB (32.40%) - LeetSync

--- 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.cpp
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     int findFinalValue(vector<int>& nums, int original) {
-        unordered_set<int> s(nums.begin(),nums.end());
-        while(s.find(original)!=s.end()){
+        unordered_map<int,bool> mpp;
+        for(auto it:nums){
+            mpp[it]=true;
+        }
+        while(mpp.find(original)!=mpp.end()){
             original*=2;
         }
         return original;
